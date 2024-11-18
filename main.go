d5b279c2159f3ed3af9c8c8c36e99a6bcf77f8bf
TomoBossi <tomasbossi97@gmail.com>
2024-11-18 19:22:41 -0300

    clusterize

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "tracer/pkg/image"
+import (
+	"fmt"
+	"tracer/pkg/cluster"
+	"tracer/pkg/image"
+)
 
 func main() {
-	pixels := image.RgbaToGrayPixels(image.LoadRgbaPixels("/home/tomo/Downloads/lain.jpg"))
-	binary := image.BinaryPixels(pixels, image.ThresholdRelativeArea{LessOrEqual: 135, GreaterDiff: 20, Radius: 13})
-	img := image.CreateBinaryImage(binary)
-	image.SavePngImage(img, "/home/tomo/Downloads/test.png")
+	pixels := image.RgbaToGrayPixels(image.LoadRgbaPixels("./assets/clusterize_medium.png"))
+	binary := image.BinaryPixels(pixels, image.ThresholdAbsolute{LessOrEqual: 150})
+	//fmt.Println(len(cluster.Clusterize(binary, 5)))
+	fmt.Println(cluster.Clusterize(binary, 5))
+	// img := image.CreateBinaryImage(binary)
+	// image.SavePngImage(img, "./out/test.png")
 }
--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -4,6 +4,66 @@ type Point struct {
 	X, Y int
 }
 
-func Clusterize(binary [][]bool, minDistance int) {}
+func Clusterize(binary [][]bool, maxDistance int) [][]Point {
+	clusters := make([][]Point, 0)
+	copy := deepCopy(binary)
+	for y := range copy {
+		for x := range copy[y] {
+			if copy[y][x] {
+				cluster := make([]Point, 0, 1)
+				neighbors := []Point{{x, y}}
+				for len(neighbors) > 0 {
+					for _, n := range neighbors {
+						copy[n.Y][n.X] = false
+					}
+					n := neighbors[0]
+					nX, nY := n.X, n.Y
+					cluster = append(cluster, n)
+					neighbors = append(neighbors[1:], scan(copy, nX, nY, maxDistance)...)
+				}
+				clusters = append(clusters, cluster)
+			}
+		}
+	}
+	return clusters
+}
 
-func scan(x, y, maxDistance int) {}
+func deepCopy(binary [][]bool) [][]bool {
+	copy := make([][]bool, len(binary))
+	for y, row := range binary {
+		copy[y] = make([]bool, len(binary[y]))
+		for x, value := range row {
+			copy[y][x] = value
+		}
+	}
+	return copy
+}
+
+func scan(binary [][]bool, x, y, maxDistance int) []Point {
+	neighbors := make([]Point, 0, 4*maxDistance*maxDistance)
+	width, height := len(binary[0]), len(binary)
+	for d := 1; d <= maxDistance; d++ {
+		nX, nY := x+d, y+d
+		for i := 0; i < d*2+1; i++ {
+			if inRange(width, height, nX-d*2, nY-i) && binary[nY-i][nX-d*2] {
+				neighbors = append(neighbors, Point{nX - d*2, nY - i})
+			}
+			if inRange(width, height, nX, nY-i) && binary[nY-i][nX] {
+				neighbors = append(neighbors, Point{nX, nY - i})
+			}
+			if i != 0 && i != d*2 {
+				if inRange(width, height, nX-i, nY) && binary[nY][nX-i] {
+					neighbors = append(neighbors, Point{nX - i, nY})
+				}
+				if inRange(width, height, nX-i, nY-d*2) && binary[nY-d*2][nX-i] {
+					neighbors = append(neighbors, Point{nX - i, nY - d*2})
+				}
+			}
+		}
+	}
+	return neighbors
+}
+
+func inRange(width, height, x, y int) bool {
+	return x > 0 && y > 0 && x < width && y < height
+}
